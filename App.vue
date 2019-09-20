601c88954f207fe892da46bc3066cda924f4c88d
1111 <1276830496@qq.com>
2019-09-20 16:03:03 +0800

    页面渲染完成

--- App.vue
+++ App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <My-Tree :dataList="data" v-if="data.length"></My-Tree>
+    <My-Tree :dataList="data" v-if="data.length" :fileDrop="fileDrop" :diectoryDrops="diectoryDrops"></My-Tree>
   </div>
 </template>
 <script>
@@ -8,7 +8,15 @@ import { getTreeList } from './api'
 import MyTree from './MyTree'
 export default {
   data () {
-    return { data: [] }
+    return {      data: [],
+      fileDrop: [
+        { text: "rm", value: '删除文件' }
+      ],
+      diectoryDrops: [
+        { text: 'rn', value: '修改文件' },
+        { text: 'rm', value: '删除文件夹' }
+      ]
+    }
   },
   components: {
     MyTree,
@@ -17,6 +25,7 @@ export default {
     let { data } = await getTreeList();
     // console.log(data); //请求到接口数据
     //1)扁平数据如何变成多层数据，递归数据
+    data.parent.forEach(p => p.type = 'parent')  //添加type属性
     let AllData = [...data.parent, ...data.child]
     // console.log(AllData)   //变成数组
     this.data = AllData;
--- MyTree.vue
+++ MyTree.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-tree :data="allData">
+  <el-tree :data="allData" default-expand-all :render-content="renderContent" :expand-on-click-node="true">
 
   </el-tree>
 </template>
@@ -10,7 +10,10 @@ export default {
     dataList: {
       type: Array,
       default: () => []
-    }
+    },
+    fileDrop: Array,
+    diectoryDrops: Array
+
   },
   data () {
     return {
@@ -25,12 +28,44 @@ export default {
     }
   },
   methods: {
+    isParent (data) {
+      return data.type === 'parent'
+    },
+    renderContent (h, { node, data, store }) {
+      let list = this.isParent(data) ? this.diectoryDrops : this.fileDrop   //判断是否是文件夹
+      // console.log(data.type)
+      // console.log(node)
+      return (
+        <div style={{ width: '100%' }}>
+          {
+            //文件夹样式判断
+            this.isParent(data) ?
+              node.expanded ?
+                <i class="el-icon-folder-opened"></i> :
+                <i class="el-icon-folder"></i> :
+              <i class="el-icon-document"></i>
+
+          }
+          {/***** <i class="el-icon-folder-opened"></i>****** */}
+          {data.name}
+          <el-dropdown placement="bottom-start" trigger="click">
+            <span class="el-dropdown-link">
+              <i class="el-icon-arrow-down el-icon--right"></i>
+            </span>
+            <el-dropdown-menu slot="dropdown">
+              {list.map(item => (
+                <el-dropdown-item>{item.value}</el-dropdown-item>
+              ))}
+            </el-dropdown-menu>
+          </el-dropdown>
+        </div >
+      )    },
     transfromData () {
       //需要根据数据进行克隆，克隆后的数据再进行操作
       let AllData = _.cloneDeep(this.dataList);  //深拷贝防止在子组件操作父组件数据
       //2)变成映射表  {id:1,{name:"文件夹1"，pid:0,id:1}}
       let treeMapList = AllData.reduce((memo, current) => {
-        current.label = current.name;
+        // current.label = current.name;
         memo[current["id"]] = current;
         return memo;
       }, {})
@@ -55,3 +90,12 @@ export default {
   }
 }
 </script>
+<style scoped>
+.el-tree {
+  width: 50%;
+}
+.el-dropdown {
+  float: right !important;
+  background: red;
+}
+</style>
